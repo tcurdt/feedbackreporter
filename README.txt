39e25f705be5b917f39556b6ef7e8d07d6ee5401
Torsten Curdt <tcurdt@vafer.org>
2008-05-19 23:20:17 +0200

    added README added LICENSE added RELEASENOTES added license headers updated the php scripts

---
+++ README.txt
@@ -0,0 +1,19 @@
+For information on how to integrate the FeedbackReporter Framework please
+see the screencast and the documentation section at
+
+ http://vafer.org/projects/feedbackreporter/
+
+Suggestions, bug reports and fixes are much welcome.
+The source code is available through git at
+
+ git://vafer.org/FeedbackReporter.git
+
+Enjoy!
+
+TODO:
+ - error handling
+ - asynch upload
+ - cancel upload
+
+Credits:  
+ - Fraser Speirs, Multipart/Form construction, BSD License
--- Server/feedback.php
+++ Server/feedback.php
@@ -1,12 +1,28 @@
 <?php
+/*
+ * Copyright 2008, Torsten Curdt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-$feedback_dir = '/home/tcurdt/feedback/';
+# destination directory needs to be writable by web server
+$feedback_dir = '/path/to/feedback/';
 
-$feedback_files = array('email');
-
-$feedback_max_files = 20;
+# fields that get stored as files
+$feedback_files = array('user', 'version', 'comment', 'email', 'system', 'console', 'crashes', 'preferences', 'shell');
 
 // --------------------------------------
+$feedback_max_files = 20;
 
 function uniq()
 {
