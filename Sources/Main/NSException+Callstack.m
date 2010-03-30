99b73c73c32ba2e6d1043452bedf568f9b2ecd88
Torsten Curdt <tcurdt@vafer.org>
2010-03-30 16:24:04 +0200

    call callStackSymbols directly (contributed by Sasmito Adibowo)

--- Sources/Main/NSException+Callstack.m
+++ Sources/Main/NSException+Callstack.m
@@ -42,9 +42,13 @@
     return addresses;
 }
 
-
 - (NSString*) my_callStack
 {
+	if ([self respondsToSelector:@selector(callStackSymbols)]) {
+		NSArray *symbols = [(id)self callStackSymbols];
+		return [NSString stringWithFormat:@"%@\n",symbols];
+	}
+
     NSArray *addresses = [self my_callStackReturnAddressesSkipping: 2 limit: 15];
 
     if (!addresses) {
@@ -71,6 +75,9 @@
     [cmd setArgs:args];
     [cmd setOutput:output];
     [cmd setError:output];
+
+    // execute returns -1 if command does not exist
+
     if([cmd execute] != 0) {
         [cmd release];
         return nil;
