62a6d7607c38138c6dc941e15977cba6bd01fbb2
Sean McBride <sean@rogue-research.com>
2017-08-02 16:55:42 -0400

    Changed FPUploader to prefer NSURL over NSString paths
    - rewrote loop using dictionary block enumeration.
    - improved error checking.
    - improved some log messages.

--- Sources/Tests/UploaderTestCase.m
+++ Sources/Tests/UploaderTestCase.m
@@ -28,7 +28,8 @@ BOOL terminated;
 {
     NSLog(@"Uploading");
 
-    FRUploader *uploader = [[FRUploader alloc] initWithTarget:@"http://vafer.org/feedback.php?project=TestCase" delegate:self];
+    NSURL* url = [NSURL URLWithString:@"http://vafer.org/feedback.php?project=TestCase"];
+    FRUploader *uploader = [[FRUploader alloc] initWithTargetURL:url delegate:self];
     
     NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
     [dict setObject:@"test" forKey:@"test"];
